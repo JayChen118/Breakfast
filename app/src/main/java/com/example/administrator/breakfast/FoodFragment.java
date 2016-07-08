16d2b681f59f9dfb6f97baee206795d6dca74066
JayChen <jay.chen@soouya.com>
2016-07-08 14:48:08 +0800

    update gitignore file

--- app/src/main/java/com/example/administrator/breakfast/FoodFragment.java
+++ app/src/main/java/com/example/administrator/breakfast/FoodFragment.java
@@ -35,7 +35,6 @@ public class FoodFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
         Log.d("Jay", "FoodFragment onCreate");
 
     }
@@ -103,8 +102,4 @@ public class FoodFragment extends Fragment {
 
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_main, menu);
-    }
 }
--- app/src/main/java/com/example/administrator/breakfast/MainActivity.java
+++ app/src/main/java/com/example/administrator/breakfast/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String SOUP = "soup";
 
+    public static final String FRUIT = "fruit";
+
+
     public static final String[] DEFAULT_BREAKFAST_LIST = {"黑米粥", "豆浆+包点", "牛奶", "玉米粥", "小米粥"};
 
-    List<Food> list = getDefaultBreakfastList();
+    public static final String[] DEFAULT_FRUIT_LIST = {"苹果", "火龙果", "梨", "桃子", "葡萄"};
+
+    public static final Map<String, String[]> DEFAULT_MAP = new HashMap<>();
+
+    static {
+        DEFAULT_MAP.put(BREAKFAST, DEFAULT_BREAKFAST_LIST);
+        DEFAULT_MAP.put(FRUIT, DEFAULT_FRUIT_LIST);
+    }
+
+    List<Food> list = getDefaultFoodList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private List<Food> getDefaultBreakfastList(){
+    private List<Food> getDefaultFoodList() {
         List<Food> result = new ArrayList<>();
         for (String name : DEFAULT_BREAKFAST_LIST) {
             Food food = new Food(name);
@@ -61,4 +77,38 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
+    private void resetFragment(String type) {
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList("list", new ArrayList<Parcelable>());
+        FoodFragment foodFragment = new FoodFragment();
+        foodFragment.setArguments(bundle);
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, foodFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.breakfast: {
+                resetFragment(BREAKFAST);
+            }
+            case R.id.fruit: {
+                resetFragment(FRUIT);
+            }
+        }
+
+        Log.d("Jay", "" + (item.getItemId() == R.id.fruit));
+        return super.onOptionsItemSelected(item);
+    }
 }
